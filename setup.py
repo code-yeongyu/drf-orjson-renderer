b7b9dfbf949d3eb4d911212404bc16844687c252
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2024-01-11 20:52:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='drf_orjson',
-    version='2.0.3',
+    version='2.0.4',
     python_requires=">=3.6",
     description='Django Rest Framework ORJSON Renderer',
     author='Gizmag,baffolobill',
     url='https://github.com/baffolobill/drf-orjson-renderer',
     packages=find_packages(exclude=["tests"]),
-    install_requires=["django<3.0", "orjson", "djangorestframework"],
+    install_requires=["django", "orjson", "djangorestframework"],
     extras_require={"dev": ["pytest", "pytest-runner", "pytest-cov", "pytest-mock"]},
 )
